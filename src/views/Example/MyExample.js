3bf8ca3304aa5a1033660aafde64664b6b10ecdf
zabaoit <nguyengiabao8407@gmail.com>
2025-05-06 20:58:19 +0700

    state

--- src/views/Example/MyExample.js
+++ src/views/Example/MyExample.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  //key: value
+  // bản chất state giúp cập nhật lại dữ liệu mà các bạn không cần f5
+  state = {
+    name: "Baobao",
+    fb: "fb.com/zabaoit",
+  };
+
+  /* 
+  JSX => return block
+  fragment
+  */
   render() {
-    return <div>Hello My Components</div>;
+    // let name = "Baobao";
+    return (
+      // <></>
+      <React.Fragment>
+        {/* <div className="abc"> */}
+        <div className="first">Hello My Components, My name is {this.state.name} </div>
+        <div className="second">facebook: {this.state.fb}</div>
+        {/* </div> */}
+      </React.Fragment>
+    );
   }
 }
 

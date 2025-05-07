97b357c999b7fc2e0056ddc0e7403eb480d1fee4
zabaoit <nguyengiabao8407@gmail.com>
2025-05-07 07:34:26 +0700

    setState

--- src/views/Example/MyExample.js
+++ src/views/Example/MyExample.js
@@ -4,8 +4,16 @@ class MyComponent extends React.Component {
   //key: value
   // bản chất state giúp cập nhật lại dữ liệu mà các bạn không cần f5
   state = {
-    name: "Baobao",
+    name: "",
     fb: "fb.com/zabaoit",
+  };
+
+  handleOnChangeName = event => {
+    //this.state.name = event.target.value; // bad code
+    // merge
+    this.setState({
+      name: event.target.value,
+    });
   };
 
   handleClickButton = () => {
@@ -22,7 +30,10 @@ class MyComponent extends React.Component {
       // <></>
       <React.Fragment>
         {/* <div className="abc"> */}
-        <div className="first">Hello My Components, My name is {this.state.name} </div>
+        <div className="first">
+          <input value={this.state.name} type="text" onChange={event => this.handleOnChangeName(event)}></input>
+          My name is {this.state.name}{" "}
+        </div>
         <div className="second">facebook: {this.state.fb}</div>
         <div className="third">
           <button onClick={() => this.handleClickButton()}>Click me</button>

336a1ebccdcd2ff32be1938015962c8517d2a246
zabaoit <nguyengiabao8407@gmail.com>
2025-05-13 15:22:55 +0700

    Setting up Redux in React app

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,18 @@
+const initState = {
+  uers: [
+    {
+      id: 1,
+      name: "BaoBao",
+    },
+    {
+      id: 2,
+      name: "Ngb",
+    },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
